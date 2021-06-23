380f76a4a28c63b994471634ae75af0d39eee82f
oneowhat <brendan.kendrick@protonmail.com>
2021-06-23 08:30:24 -0700

    Added app class & requirements

---
+++ examples/amusement-park-tickets/src/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        // Amusement park tickets
+        // $10 - adult
+        // $7 - students
+        // $5 - kids (12 - 2)
+        // $1 - senior (65 or older)
+        // $0 - 2 or less
+
+        // Get a name
+        // Ask how many people
+        // Ask age per person
+        // Show total price (2 decimal places)
+
+        Scanner console = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = console.nextLine();
+
+
+    }
+}
