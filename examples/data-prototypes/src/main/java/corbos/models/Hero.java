607f4512c60dfee9ea0b8dfe0ba111523ff48c52
Corbin March <corbin.march@gmail.com>
2021-07-21 21:19:48 -0500

    add a few tests ;

--- examples/data-prototypes/src/main/java/corbos/models/Hero.java
+++ examples/data-prototypes/src/main/java/corbos/models/Hero.java
@@ -1,8 +1,18 @@
 package corbos.models;
 
+import java.util.Objects;
+
 public class Hero {
     private int id;
     private String name;
+
+    public Hero() {
+    }
+
+    public Hero(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public int getId() {
         return id;
@@ -18,5 +28,18 @@ public class Hero {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hero hero = (Hero) o;
+        return id == hero.id && Objects.equals(name, hero.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
