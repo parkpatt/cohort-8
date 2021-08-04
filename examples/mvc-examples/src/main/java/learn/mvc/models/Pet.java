4d863c88a181bb4300763d6e851d074f392efd1f
oneowhat <brendan.kendrick@protonmail.com>
2021-08-04 09:44:55 -0700

    Added mvc examples

---
+++ examples/mvc-examples/src/main/java/learn/mvc/models/Pet.java
@@ -0,0 +1,29 @@
+package learn.mvc.models;
+
+public class Pet {
+    private int id;
+    private String name;
+    private String type;
+
+    public Pet(int id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
