146ddd7ec63934f340e095b2d41ede9764588ff4
Parker <parkpatt@gmail.com>
2021-08-19 13:41:51 -0500

    ?

--- examples/op-heroes/src/components/ViewHeroes.js
+++ examples/op-heroes/src/components/ViewHeroes.js
@@ -7,22 +7,6 @@ import { fetchAll } from '../api/heroApi';
 function ViewHeroes() {
 
   const [heroes, setHeroes] = useState([]);
-<<<<<<< HEAD
-
-  const fetchAll = async () => {
-    try {
-      const response = await fetch(url);
-      if (response.status !== 200) {
-        return Promise.reject(new Error("Fetch failed."));
-      }
-      const json = await response.json();
-      setHeroes(json);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-=======
->>>>>>> 6806cda2ce7ae8e8cd48f2da913a5245a34bbbe2
 
   useEffect(() => {
     fetchAll()
@@ -35,12 +19,7 @@ function ViewHeroes() {
       <div className="row align-items-center">
         <h2 className="col">Heroes</h2>
         <div className="col  align-self-end">
-<<<<<<< HEAD
-          <Link to='/add' className="btn btn-primary">Add</Link>
-          {/* <button type="button" className="btn btn-primary" onClick={onAdd}>Add</button> */}
-=======
           <Link to="/add" className="btn btn-primary">Add</Link>
->>>>>>> 6806cda2ce7ae8e8cd48f2da913a5245a34bbbe2
         </div>
       </div>
       {heroes.map(h => <HeroRow key={h.id} hero={h} />)}
